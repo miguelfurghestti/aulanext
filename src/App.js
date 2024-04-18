d4dcdee6b7f9921b6d6ed2c9ba7ea31d965b5961
Miguel S. Furghestti <miguel.aghei@hotmail.com>
2024-04-18 11:42:49 -0300

    usando fetch consumindo api e exibindo resultados com map

--- src/App.js
+++ src/App.js
@@ -1,79 +1,38 @@
-import { useState } from "react";
-import MostrarNome from "./components/MostrarNome";
+import { useEffect, useState } from "react";
+import "./style.css";
 
 function App() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [idade, setIdade] = useState("");
-  // const [user, setUser] = useState({
-  //   nome: "Fulano",
-  //   email: "fulano@gmail.com",
-  //   idade: 11,
-  // });
+  const [nutri, setNutri] = useState([]);
 
-  // ou começar como um objeto vázio:
-  const [user, setUser] = useState({});
+  useEffect(() => {
+    let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
 
-  function handleChangeName(nome) {
-    setNome(nome);
-  }
-
-  function handleRegister(e) {
-    e.preventDefault();
-
-    setUser({
-      nome: nome,
-      email: email,
-      idade: idade,
-    });
-  }
-
+    fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        console.log(json);
+        setNutri(json);
+      });
+  }, []);
   return (
-    <div>
-      <h1>Bem vindo ao meu projeto!</h1>
-      <h2>Olá: {nome}</h2>
-      {/* <MostrarNome nome="Miguel Furghestti" cargo="Admin" /> */}
-      {/* <button onClick={handleChangeName("Miguel Furghestti")}> */}
-      {/* Quando eu abrir parenteses após uma função, o navegador executa ela, como se eu estivesse chamando ela, para que isso não ocorra, utilizamos uma função anonima, dessa forma ela só será acionada mediante ação. */}
-      <button onClick={() => handleChangeName("Miguel Furghestti")}>
-        Mudar Nome
-      </button>
-      <h1>Cadastrando usuário</h1>
-      <form onSubmit={handleRegister}>
-        <label>Nome:</label>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
+      </header>
 
-        <input
-          type="text"
-          placeholder="Digite seu nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-        <br />
-        <label>E-mail:</label>
+      {nutri.map((item) => {
+        return (
+          <article key={item.id} className="post">
+            <strong className="titulo">{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className="capa" />
+            <p className="subtitulo">{item.subtitulo}</p>
 
-        <input
-          type="text"
-          placeholder="Digite seu e-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <label>Idade:</label>
-
-        <input
-          type="text"
-          placeholder="Digite sua idade"
-          value={idade}
-          onChange={(e) => setIdade(e.target.value)}
-        />
-        <br />
-        <button type="submit">Registrar</button>
-      </form>
-      Nome: {user.nome}
-      <br />
-      E-mail: {user.email}
-      <br />
-      Idade: {user.idade}
+            <a href="" className="botao">
+              Acessar
+            </a>
+          </article>
+        );
+      })}
     </div>
   );
 }
